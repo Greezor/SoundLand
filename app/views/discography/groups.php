036cff887b65a97bb4e119f7d611dff40fc1e2b7
Leon2462 <saintmupip@gmail.com>
2019-12-15 20:51:07 +0300

    add view for groups

---
+++ app/views/discography/groups.php
@@ -0,0 +1,12 @@
+<div class="card" align="center" style="width: 50rem;margin:50px auto; background-color:#FFFFFF99;border: 6px solid #00000099;padding: 10px; border-radius: 25px;">
+ <h5 class="card-title">Личный кабинет</h5>
+    <div class="card-body" align="left>
+
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">Группа 1</a>
+            <a href="#" class="list-group-item list-group-item-action">Группа 2</a>
+            <a href="#" class="list-group-item list-group-item-action">Группа 3</a>
+        </div>
+
+    </div>
+</div>
