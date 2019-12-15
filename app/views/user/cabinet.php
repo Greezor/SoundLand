79c705c7a430d0cc2ec4d4361a3331fac3cb72f3
Leon2462 <saintmupip@gmail.com>
2019-12-15 20:51:38 +0300

    add view for cabinet

---
+++ app/views/user/cabinet.php
@@ -0,0 +1,10 @@
+<div class="card" align="center" style="width: 50rem;margin:50px auto; background-color:#FFFFFF99;border: 6px solid #00000099;padding: 10px; border-radius: 25px;">
+ <h5 class="card-title">Личный кабинет</h5>
+    <div class="card-body" align="left">
+    <p><span>Аватар: </span> <img src="/assets/images/user.svg" style="width: 100px;"></img></p>
+    <p><span>Почта: </span></p>
+    <p><span>Никнейм: </span></p>
+    <p><span>Роль: </span></p>
+    <a href="#" class="btn btn-dark mr-sm-2" style="color:#FFFFFF;">Редактировать данные</a>
+    </div>
+</div>
