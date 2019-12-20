af8be68e2f245f712a2807aebed4c7287ad6322e
Leon2462 <saintmupip@gmail.com>
2019-12-20 13:21:31 +0300

    delete view cabinet from folder cabinet

--- app/views/page/cabinet.php
+++
@@ -1,8 +0,0 @@
-<div class="card" align="center" style="width: 50rem;margin:50px auto; background-color:#FFFFFF99;border: 6px solid #00000099;padding: 10px; border-radius: 25px;">
- <h5 class="card-title">Личный кабинет</h5>
-    <div class="card-body">
-    <span>Почта: </span>
-    <span>Логин: </span>
-
-    </div>
-</div>
