9c2e51cbc9fab23e3d550d7fa243402757a1d24c
Greezor <dev.greezor@gmail.com>
2019-12-26 01:23:00 +0300

    fix Windows html duplicating bug

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -14,6 +14,7 @@ class Controller extends BaseClass
         extract($params);
 
         if( $templateWrapper ){
+            ob_clean();
             ob_start();
             require __DIR__ . "/../templates/{$this->template}.php";
             $output = ob_get_contents();
